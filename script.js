089ba7917fa01a678cdef5ba42b876283403a7cc
asminkarki012 <asminkarki09@gmail.com>
2022-11-11 21:50:08 +0545

    added edit and recent post top features

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 let myTasks = [];
 const todoTask = document.getElementById("new-task-input");
+
+// Blink cursor in input field initially
+todoTask.focus();
+
 const submitBtn = document.getElementById("new-task-submit");
 
 submitBtn.addEventListener("click", addTask);
 //to add task in task variable
 function addTask(event) {
   event.preventDefault();
-  let tasks = [];
   if (!todoTask.value) {
     const errorMsg = document.getElementById("errormsg");
     errorMsg.innerHTML = "Please Insert task first!!!!";
   } else {
-
     const errorMsg = document.getElementById("errormsg");
     errorMsg.innerHTML = "";
-    let task = { name: todoTask.value, status: true };
-    tasks.push(task);
-    myTasks.push(task);
+
+    var count = 0;
+    const task = { id: count, name: todoTask.value, status: false };
+    count++;
+
+    //unshift add task front of array
+    myTasks.unshift(task);
     todoTask.value = "";
     setData();
-    displayTask(tasks);
-    
+
+    displayTask();
+    window.location.reload();
   }
 }
 
-const displayTask = (tasks) => {
-  //where to store the data
+const displayTask = () => {
+  //where to append the new task child @tbody
   const todoList = document.getElementById("todo-list");
-  for (let i in tasks) {
+
+  for (let i in myTasks) {
     // create todo row
     const newtodoRow = document.createElement("tr");
     newtodoRow.classList.add(
@@ -36,34 +44,43 @@ const displayTask = (tasks) => {
       "text-orange-700"
     );
 
-    newtodoRow.setAttribute("id", `todo-row${i}`);
+    // newtodoRow.setAttribute("id", `todo-row${myTasks[i].id}`);
 
     //create todo data for table
     const newtodoData = document.createElement("td");
-    newtodoData.innerHTML = tasks[i].name;
+    newtodoData.innerHTML = myTasks[i].name;
 
+    // id to edit the task inside table
+    newtodoData.setAttribute("id", `todo${myTasks[i].id}`);
     newtodoData.classList.add("todo");
+
     //create todo data for button
     const newtodoDataBtn = document.createElement("td");
 
+    // to set edit delete and check in same row
+    newtodoDataBtn.classList.add("flex", "gap-4", "m-auto");
+
     //buttons for status
     //default tick button
+    //status is false when task is not complete;
     const statusBtn = document.createElement("button");
 
-      if (tasks[i].status === false) {
-        statusBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    if (myTasks[i].status === true) {
+      // cross sign
+      statusBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                       </svg>`;
 
-        newtodoData.classList.add('inactive');
-      } else {
-        statusBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+      newtodoData.classList.add("inactive");
+    } else {
+      //tick sign
+      statusBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                       </svg>`;
 
-        newtodoData.classList.remove('inactive');
+      newtodoData.classList.remove("inactive");
     }
 
     // statusBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -73,34 +90,65 @@ const displayTask = (tasks) => {
 
     //toggle for status
     //true -> not completed
-    statusBtn.classList.add("px-4");
+    // statusBtn.classList.add("px-4");
     statusBtn.addEventListener("click", () => {
-      if (tasks[i].status === true) {
-        statusBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                      </svg>`;
-        tasks[i].status = false;
-        newtodoData.classList.add('inactive');
-        setData();
-      } else {
+      if (myTasks[i].status === true) {
+        //tick sign
         statusBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                       </svg>`;
-        tasks[i].status = true;
-        newtodoData.classList.remove('inactive');
+        myTasks[i].status = false;
+        newtodoData.classList.remove("inactive");
+        setData();
+      } else {
+        // cross sign
+        statusBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                      </svg>`;
+
+        myTasks[i].status = true;
+        newtodoData.classList.add("inactive");
         setData();
       }
     });
 
     newtodoDataBtn.appendChild(statusBtn);
 
+    //button for edit
+
+    const editBtn = document.createElement("button");
+
+    editBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+    </svg>
+    `;
+    // editBtn.classList.add("px-4", "m-auto");
+    let flag = true;
+    editBtn.addEventListener("click", () => {
+      if (flag) {
+        newtodoData.setAttribute("contenteditable", "true");
+        flag = false;
+        editBtn.classList.toggle("text-blue-500");
+      } else {
+        newtodoData.setAttribute("contenteditable", "false");
+        flag = true;
+        const editedText = document.getElementById(`todo${myTasks[i].id}`);
+        newtodoData.innerHTML = editedText.textContent;
+        myTasks[i].name = editedText.textContent;
+        setData();
+        editBtn.classList.toggle("text-blue-500");
+      }
+    });
+
+    newtodoDataBtn.appendChild(editBtn);
+
     //button for trash
 
     const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("px-4");
-    deleteBtn.setAttribute("id", `todo-row${i}`);
+
+    deleteBtn.setAttribute("id", `todo-row${myTasks[i].id}`);
     deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -109,11 +157,9 @@ const displayTask = (tasks) => {
 
     deleteBtn.addEventListener("click", () => {
       let deleteId = deleteBtn.getAttribute("id");
-      myTasks.splice(deleteId,1); 
+      myTasks.splice(deleteId, 1);
       newtodoRow.remove();
       setData();
-
-
     });
     newtodoDataBtn.appendChild(deleteBtn);
 
@@ -124,22 +170,19 @@ const displayTask = (tasks) => {
     //append to table
     todoList.appendChild(newtodoRow);
 
-
-
-  //   const countTodo = myTasks.filter((x) => {
-  //    x.status===false});
-  //    console.log(countTodo);
-  // console.log(countTodo.length);
+    //   const countTodo = myTasks.filter((x) => {
+    //    x.status===false});
+    //    console.log(countTodo);
+    // console.log(countTodo.length);
   }
 };
 
 const setData = () => {
   localStorage.setItem("myTasks", JSON.stringify(myTasks));
-
+  // Reset global variable after setting up
 };
 
-
-function restoreData(){
+function restoreData() {
   if (!localStorage.myTasks) {
     displayTask(myTasks);
   } else {
@@ -149,10 +192,46 @@ function restoreData(){
     myTasks = storedObject;
     displayTask(myTasks);
   }
-};
+}
 
 //restore data from localstorage
 restoreData();
 
 //clear local storage
-// localStorage.clear();
+function clearLocalStorage() {
+  localStorage.clear();
+  window.location.reload();
+}
+
+//to implement search and resetsearch
+
+// function searchData() {
+//   // e.preventDefault();
+//   const searchInput = document.getElementById("search-input").value;
+
+//   let filter = searchInput.toUpperCase();
+//   let tbody = document.getElementById("todo-list");
+//   let tr = tbody.getElementsByTagName("tr");
+
+//   // Loop through all list items, and hide those who don't match the search query
+//   for (i = 0; i < tr.length; i++) {
+//     let td = tr[i].getElementsByTagName("td")[0];
+//     txtValue = td.textContent || td.innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       tr[i].style.display = "";
+//     } else {
+//       tr[i].style.display = "none";
+//     }
+//   }
+// }
+
+// function resetSearchData() {
+//   let tbody = document.getElementById("todo-list");
+//   let tr = tbody.getElementsByTagName("tr");
+//   const clearSearch = document.getElementById("search-input");
+//   clearSearch.value = "";
+
+//   for (i = 0; i < tr.length; i++) {
+//     tr[i].style.display = "";
+//   }
+// }
